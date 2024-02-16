05e08a7374983bd28188286788cc35a57a58e89a
Chidiebere Uduh <udugreg@gmail.com>
2024-02-16 22:03:08 +0100

    I calculated and compared dog years to human years

---
+++ dog-years/app.js
@@ -0,0 +1,20 @@
+// create a variable named myAge with a value 33
+const myAge = 20; 
+// create a variable named earlyYears named 2
+let earlyYears = 2;
+earlyYears * 10.5;
+earlyYears = (earlyYears * 10.5);
+// create a variable named laterYears and assign it a value from the subtraction of myAge from 2
+let laterYears 
+laterYears = (myAge - 2)
+// multiply laterYears by 4 and re-assign it back to later years
+laterYears = (laterYears * 4)
+console.log(earlyYears)
+console.log(laterYears)
+// declare a variable named myAgeInDogYears and assign it to a value from the addition of earlyYears and laterYears 
+let myAgeInDogYears = (earlyYears + laterYears)
+// create a variable named myName and assign it Chidosky then use built in .tolowercase
+const myName = 'Chidosky'
+'Chidosky'.toLowerCase()
+// interpolate this text with the varibles
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
