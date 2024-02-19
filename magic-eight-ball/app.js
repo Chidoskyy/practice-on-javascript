4a76dfdbbc59b7261f4447ffc7cf708097e3f511
Chidiebere Uduh <udugreg@gmail.com>
2024-02-19 19:42:56 +0100

    I changed the manipulation of the eight ball from switch case to else if statement

--- magic-eight-ball/app.js
+++ magic-eight-ball/app.js
@@ -4,6 +4,26 @@ const userQuestion = 'what question to ask on magic eight ball?';
 console.log(`${userName} asked ${userQuestion}`);
 const randomNumber = Math.floor(Math.random() *8);
 let eightBall = '';
+if (randomNumber === 0) {
+    eightBall = 'It is certain';
+  } else if (randomNumber === 1) {
+    eightBall = 'Signs point to yes'
+  } else if(randomNumber === 2){
+    eightBall = 'It is decidedly so';
+  } else if (randomNumber === 3) {
+    eightBall = 'Reply hazy try again';
+  } else if (randomNumber === 4) {
+    eightBall = 'Cannot predict now';
+  } else if (randomNumber === 5 ) {
+     eightBall ='Do not count on it';
+  } else if (randomNumber === 6) {
+    eightBall = 'My sources say no';
+  } else if (randomNumber === 7) {
+    eightBall = 'Outlook not so good';
+  };
+  console.log(eightBall)
+
+  /*
 switch (randomNumber) {
   case 0: eightBall = 'It is certain';
   break;
@@ -24,24 +44,6 @@ switch (randomNumber) {
   default: eightBall = 'No possible outcome'
 };
 console.log(eightBall)
-
-/*
-if (randomNumber === 0) {
-  eightBall = 'It is certain';
-} else if (randomNumber === 1) {
-  eightBall = 'Signs point to yes'
-} else if(randomNumber === 2){
-  eightBall = 'It is decidedly so';
-} else if (randomNumber === 3) {
-  eightBall = 'Reply hazy try again';
-} else if (randomNumber === 4) {
-  eightBall = 'Cannot predict now';
-} else if (randomNumber === 5 ) {
-   eightBall ='Do not count on it';
-} else if (randomNumber === 6) {
-  eightBall = 'My sources say no';
-} else if (randomNumber === 7) {
-  eightBall = 'Outlook not so good';
-};
 */
+
 
